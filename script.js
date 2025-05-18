f882d67e8268bd8f1b38610af40ae435104245a7
Subhana Saman <subhanariaz12@gmail.com>
2025-05-18 15:13:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Question No 1
+alert( 'Welcome To Are Website');
+
+// // Question No 2
+// alert('Error!Please enter a valid password.');
+
+// // Question No 3
+// alert('Welcome to JS Land...' + '\n' + 'Happy Coding!')
+
+// // Question No 4
+// alert('Welcome to JS Land...')
+// alert('Happy Coding!' +'\n'+' ☐ Prevent this  page from creating additional dialogs.')
+
+// // Question No 5
+// alert("Hello... I can run JS through  my web browser's console");
